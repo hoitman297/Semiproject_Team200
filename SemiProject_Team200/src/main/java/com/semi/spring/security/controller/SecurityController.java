296523b601b11e8640c0bcf53f453569158fbcd7
DESKTOP-4I0Q9AM\user1 <skan64554@gmail.com>
2026-03-18 17:05:20 +0900

    일부 html jsp변환 및 security-context.xml 추가 및 설정 , boardController 설정

--- SemiProject_Team200/src/main/java/com/semi/spring/security/controller/SecurityController.java
+++ SemiProject_Team200/src/main/java/com/semi/spring/security/controller/SecurityController.java
@@ -1,9 +1,11 @@
 package com.semi.spring.security.controller;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.semi.spring.security.controller.SecurityController;
+import com.semi.spring.member.service.MemberService;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -12,4 +14,23 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 public class SecurityController {
 
+	// 필드방식 의존성 주입
+	//@Autowired
+	private MemberService mService;
+	
+	//@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+		
+	// 생성자에 의해 자동 의존성 주입 방식
+	// 단, 생성자가 여러개면 @Autowired어노테이션 필요
+	public SecurityController(MemberService mService , BCryptPasswordEncoder passwordEncoder) {
+		this.mService = mService;
+		this.passwordEncoder = passwordEncoder;
+	}
+	
+	@RequestMapping("/accessDenied")
+	public String accessDenied(Model m) {
+		m.addAttribute("errorMsg","접근 불가능한 페이지입니다.");
+		return "common/errorPage";
+	}
 }
